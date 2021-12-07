a8521af212ae172971482dd9db2af3b07960c169
kazuma007 <28896927+kazuma007@users.noreply.github.com>
2021-12-07 09:06:44 +0900

    first commit

---
+++ src/main/kotlin/com/example/positivesns/PositiveSnsApplication.kt
@@ -0,0 +1,11 @@
+package com.example.positivesns
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PositiveSnsApplication
+
+fun main(args: Array<String>) {
+	runApplication<PositiveSnsApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/positivesns/PositiveSnsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.positivesns
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PositiveSnsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
