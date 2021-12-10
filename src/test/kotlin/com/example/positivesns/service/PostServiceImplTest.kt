3bf2901f2f34d6c9ee51a64d229dbe8f8b0618ab
kazuma007 <28896927+kazuma007@users.noreply.github.com>
2021-12-11 06:21:36 +0900

    :rotating_light: created a test for createPost()

---
+++ src/test/kotlin/com/example/positivesns/service/PostServiceImplTest.kt
@@ -0,0 +1,53 @@
+package com.example.positivesns.service
+
+import com.example.positivesns.model.dynamo.Post
+import com.example.positivesns.repository.PostRepository
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import java.time.ZoneId
+import java.time.ZonedDateTime
+
+@ExtendWith(MockKExtension::class)
+internal class PostServiceImplTest {
+
+    @InjectMockKs
+    private lateinit var postService: PostServiceImpl
+
+    @MockK
+    private lateinit var datetimeService: DatetimeService
+
+    @MockK
+    private lateinit var postRepository: PostRepository
+
+    @MockK
+    private lateinit var uuidService: UuidService
+
+    @Test
+    fun createPost() {
+        every {
+            datetimeService.getCurrentTime()
+        } returns ZonedDateTime.of(2021, 12, 11, 6, 5, 20, 0, ZoneId.of("Asia/Tokyo"))
+
+        every {
+            uuidService.getUuid()
+        } returns "41b28baf93374cf5849b4076c56783dd"
+
+        val actual = postService.createPost("test-user-1", "sample test")
+
+        val expected = Post(
+            userId = "test-user-1",
+            text = "sample test",
+            postId = "41b28baf93374cf5849b4076c56783dd",
+            registeredTime = 1639170320000,
+            updatedTime = 1639170320000,
+        )
+
+        Assertions.assertEquals(expected, actual)
+    }
+
+}
