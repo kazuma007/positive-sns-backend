a284e1d7974421ecd132ec2664d79fcf0d45f2b3
kazuma007 <28896927+kazuma007@users.noreply.github.com>
2021-12-11 05:05:45 +0900

    :heavy_plus_sign: added aws-java-sdk

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,8 +21,17 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+	// https://mvnrepository.com/artifact/com.amazonaws/aws-java-sdk
+	implementation("com.amazonaws:aws-java-sdk:1.12.128")
+
 	providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+	// https://mvnrepository.com/artifact/org.junit.jupiter/junit-jupiter-api
+	testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+
 }
 
 tasks.withType<KotlinCompile> {
