768c37f135d77cb4d2403cc78364c602535fc966
kazuma007 <28896927+kazuma007@users.noreply.github.com>
2021-12-11 20:27:08 +0900

    :rotating_light: created a test for auth()

--- src/test/kotlin/com/example/positivesns/service/UserServiceImplTest.kt
+++ src/test/kotlin/com/example/positivesns/service/UserServiceImplTest.kt
@@ -2,6 +2,7 @@ package com.example.positivesns.service
 
 import com.example.positivesns.model.dynamo.User
 import com.example.positivesns.repository.UserRepository
+import com.example.positivesns.response.user.UserResponse
 import io.mockk.confirmVerified
 import io.mockk.every
 import io.mockk.impl.annotations.InjectMockKs
@@ -36,7 +37,7 @@ internal class UserServiceImplTest {
 
         every {
             passwordService.createHashedPassword("password")
-        } returns "password"
+        } returns "hashed-password"
 
         every {
             datetimeService.getCurrentTime()
@@ -86,7 +87,7 @@ internal class UserServiceImplTest {
 
         every {
             passwordService.createHashedPassword("password")
-        } returns "password"
+        } returns "hashed-password"
 
         every {
             datetimeService.getCurrentTime()
@@ -124,5 +125,79 @@ internal class UserServiceImplTest {
         confirmVerified(userRepository)
         confirmVerified(passwordService)
         confirmVerified(datetimeService)
+    }
+
+    @Test
+    fun auth_whenUserIdDoesNotExist() {
+        every {
+            userRepository.getUser("user-id")
+        } returns null
+
+        val actual = userService.auth("user-id", "password")
+
+        val expected = UserResponse()
+
+        Assertions.assertEquals(expected, actual)
+
+        verify(exactly = 1) { userRepository.getUser("user-id") }
+        confirmVerified(userRepository)
+    }
+
+    @Test
+    fun auth_whenPasswordDoesNotMatch() {
+        every {
+            userRepository.getUser("user-id")
+        } returns User(
+            userId = "user-id",
+            password = "hashed-password",
+        )
+
+        every {
+            passwordService.isSamePassword("password", "hashed-password")
+        } returns false
+
+        val actual = userService.auth("user-id", "password")
+
+        val expected = UserResponse()
+
+        Assertions.assertEquals(expected, actual)
+
+        verify(exactly = 1) { userRepository.getUser("user-id") }
+        verify(exactly = 1) { passwordService.isSamePassword("password", "hashed-password") }
+        confirmVerified(userRepository)
+        confirmVerified(passwordService)
+    }
+
+    @Test
+    fun auth_whenSuccess() {
+        every {
+            userRepository.getUser("user-id")
+        } returns User(
+            userId = "user-id",
+            username = "username",
+            password = "hashed-password",
+            registeredTime = 1639170320000,
+            updatedTime = 1639170320000,
+        )
+
+        every {
+            passwordService.isSamePassword("password", "hashed-password")
+        } returns true
+
+        val actual = userService.auth("user-id", "password")
+
+        val expected = UserResponse(
+            userId = "user-id",
+            username = "username",
+            registeredTime = ZonedDateTime.of(2021, 12, 11, 6, 5, 20, 0, ZoneId.of("Asia/Tokyo")),
+            updatedTime = ZonedDateTime.of(2021, 12, 11, 6, 5, 20, 0, ZoneId.of("Asia/Tokyo")),
+        )
+
+        Assertions.assertEquals(expected, actual)
+
+        verify(exactly = 1) { userRepository.getUser("user-id") }
+        verify(exactly = 1) { passwordService.isSamePassword("password", "hashed-password") }
+        confirmVerified(userRepository)
+        confirmVerified(passwordService)
     }
 }
