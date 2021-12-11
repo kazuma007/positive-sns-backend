f2b9c333a886d2e830dceac1624ca363a0cd17d1
kazuma007 <28896927+kazuma007@users.noreply.github.com>
2021-12-11 12:05:25 +0900

    :hammer: changed the table setting

--- src/main/kotlin/com/example/positivesns/model/dynamo/Post.kt
+++ src/main/kotlin/com/example/positivesns/model/dynamo/Post.kt
@@ -2,23 +2,22 @@ package com.example.positivesns.model.dynamo
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBRangeKey
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable
 
 @DynamoDBTable(tableName = "post")
 data class Post(
-    @DynamoDBHashKey(attributeName = "user_id")
-    var userId: String = "",
-
-    @DynamoDBRangeKey(attributeName = "post_id")
+    @DynamoDBHashKey(attributeName = "post_id")
     var postId: String = "",
+
+    @DynamoDBAttribute(attributeName = "user_id")
+    var userId: String = "",
 
     @DynamoDBAttribute(attributeName = "text")
     var text: String = "",
 
     @DynamoDBAttribute(attributeName = "registered_time")
-    var registeredTime: Long = 0,
+    var registeredTime: Long? = null,
 
     @DynamoDBAttribute(attributeName = "updated_time")
-    var updatedTime: Long = 0,
+    var updatedTime: Long? = null,
 )
