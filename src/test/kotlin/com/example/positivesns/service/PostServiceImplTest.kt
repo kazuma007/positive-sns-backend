0bde55429fe7c2305f8084ceca41d81fe005e71e
kazuma007 <28896927+kazuma007@users.noreply.github.com>
2021-12-11 10:16:48 +0900

    :rotating_light: create a test for getPosts(userId)

--- src/test/kotlin/com/example/positivesns/service/PostServiceImplTest.kt
+++ src/test/kotlin/com/example/positivesns/service/PostServiceImplTest.kt
@@ -2,10 +2,13 @@ package com.example.positivesns.service
 
 import com.example.positivesns.model.dynamo.Post
 import com.example.positivesns.repository.PostRepository
+import com.example.positivesns.response.post.PostGetResponse
+import io.mockk.confirmVerified
 import io.mockk.every
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
+import io.mockk.verify
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
@@ -48,6 +51,103 @@ internal class PostServiceImplTest {
         )
 
         Assertions.assertEquals(expected, actual)
+
+        verify(exactly = 1) { datetimeService.getCurrentTime() }
+        verify(exactly = 1) { uuidService.getUuid() }
+        confirmVerified(datetimeService)
+        confirmVerified(uuidService)
     }
 
+    @Test
+    fun getPosts_withUserId() {
+        every {
+            postRepository.getPosts("sample-user")
+        } returns listOf(
+            Post(
+                userId = "sample-user",
+                text = "sample test 1",
+                postId = "41b28baf93374cf5849b4076c56783dd",
+                registeredTime = 1639170320000,
+                updatedTime = 1639170320000,
+            ),
+            Post(
+                userId = "sample-user",
+                text = "sample test 2",
+                postId = "5849b4076c56783dd41b28baf93374cf",
+                registeredTime = 1639170330000,
+                updatedTime = 1639170330000,
+            ),
+        )
+
+        val expected = listOf(
+            PostGetResponse(
+                userId = "sample-user",
+                text = "sample test 1",
+                postId = "41b28baf93374cf5849b4076c56783dd",
+                registeredTime = ZonedDateTime.of(2021, 12, 11, 6, 5, 20, 0, ZoneId.of("Asia/Tokyo")),
+                updatedTime = ZonedDateTime.of(2021, 12, 11, 6, 5, 20, 0, ZoneId.of("Asia/Tokyo")),
+            ),
+            PostGetResponse(
+                userId = "sample-user",
+                text = "sample test 2",
+                postId = "5849b4076c56783dd41b28baf93374cf",
+                registeredTime = ZonedDateTime.of(2021, 12, 11, 6, 5, 30, 0, ZoneId.of("Asia/Tokyo")),
+                updatedTime = ZonedDateTime.of(2021, 12, 11, 6, 5, 30, 0, ZoneId.of("Asia/Tokyo")),
+            ),
+        )
+
+        val actual = postService.getPosts("sample-user")
+
+        Assertions.assertEquals(expected, actual)
+
+        verify(exactly = 1) { postRepository.getPosts("sample-user") }
+        confirmVerified(postRepository)
+    }
+
+    @Test
+    fun getPosts_withoutUserId() {
+        every {
+            postRepository.getPosts()
+        } returns listOf(
+            Post(
+                userId = "sample-user-1",
+                text = "sample test 1",
+                postId = "41b28baf93374cf5849b4076c56783dd",
+                registeredTime = 1639170320000,
+                updatedTime = 1639170320000,
+            ),
+            Post(
+                userId = "sample-user-2",
+                text = "sample test 2",
+                postId = "5849b4076c56783dd41b28baf93374cf",
+                registeredTime = 1639170330000,
+                updatedTime = 1639170330000,
+            ),
+        )
+
+        val expected = listOf(
+            PostGetResponse(
+                userId = "sample-user-1",
+                text = "sample test 1",
+                postId = "41b28baf93374cf5849b4076c56783dd",
+                registeredTime = ZonedDateTime.of(2021, 12, 11, 6, 5, 20, 0, ZoneId.of("Asia/Tokyo")),
+                updatedTime = ZonedDateTime.of(2021, 12, 11, 6, 5, 20, 0, ZoneId.of("Asia/Tokyo")),
+            ),
+            PostGetResponse(
+                userId = "sample-user-2",
+                text = "sample test 2",
+                postId = "5849b4076c56783dd41b28baf93374cf",
+                registeredTime = ZonedDateTime.of(2021, 12, 11, 6, 5, 30, 0, ZoneId.of("Asia/Tokyo")),
+                updatedTime = ZonedDateTime.of(2021, 12, 11, 6, 5, 30, 0, ZoneId.of("Asia/Tokyo")),
+            ),
+        )
+
+        val userId = null
+        val actual = postService.getPosts(userId)
+
+        Assertions.assertEquals(expected, actual)
+
+        verify(exactly = 1) { postRepository.getPosts() }
+        confirmVerified(postRepository)
+    }
 }
